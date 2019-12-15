ce13d01d3aa228d058c8aef198d761481a35590d
Sumerin <sumekpl@gmail.com>
2019-12-15 15:53:05 +0100

    bind

--- BindFunction.fsx
+++ BindFunction.fsx
@@ -7,6 +7,15 @@ let h b =
     if b < 0 then None else Some b
 
 
+
+let result = Some 5 |> f |> h
+
 let result1 = Some -1 |> Option.bind f |> Option.bind h 
 let result2 = Some 0 |> Option.bind f |> Option.bind h 
 let result3 = Some 2 |> Option.bind f |> Option.bind h 
+
+let (>>=) a b = Option.bind b a
+
+let result1 = Some -1 >>= f >>= h 
+let result2 = Some 0 >>= f >>=  h 
+let result3 = Some 2 >>= f >>=  h 
